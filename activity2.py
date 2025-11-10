f2ad5e1e3fa4546645faa53d69642f2105b46cb8
Khushi-codingal <rashmiabc@gmail.com>
2025-11-10 18:42:37 +0530

    not operator

---
+++ activity2.py
@@ -0,0 +1,18 @@
+a = 10
+b = 12
+c = 12
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = "coding"
+if a != b:
+    print("a and b are diffirent")
+
+
+a = int(input("enter a number:"))
+if a%2  != 0:
+    print("a is not even")
+else :
+    print("a is even")
